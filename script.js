4613ebeddcf8a06a15cc40add0ddee8daf8ee972
Mikel Severson <mikelseverson@Mikels-MacBook-Air.local>
2015-07-22 09:15:45 -0500

    Fixed display of cash and dom currectly displaying values

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
+}
 
+function formatNum (num) {
+	num /= 100;
+	num = parseFloat(Math.round(num * 100) / 100).toFixed(2);
+	num = num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return num;
 }
 
 var User = {
 	numCurrentApples : 0,
 	numCurrentOranges : 0,
 	numCurrentBananas : 0,
-	//numCurrentGrapes : 0,
 	numCurrentPears : 0,
+
 	totalNumApples : 0,
 	totalNumOranges : 0,
 	totalNumBananas : 0,
-	//totalNumGrapes : 0,
 	totalNumPears : 0,
+
 	spentApples : 0,
 	spentOranges : 0,
 	spentBananas : 0,
-	//spentGrapes : 0,
 	spentPears : 0,
+
 	cash : 10000
 }
 
@@ -27,7 +33,6 @@ var userBasket = {
 	apples: 0,
 	oranges: 0,
 	bananas: 0,
-	grapes: 0,
 	pears: 0
 }
 
@@ -37,11 +42,8 @@ var fruitPrices = [
 	["apples", 50, ""], 
 	["oranges", 50, ""],
 	["bananas", 50, ""],
-	//["grapes", 950, ""],
 	["pears", 950, ""]
 ]
-
-var change = "";
 
 //function that changes price for each fruit
 function fruitPriceUpdates() {
@@ -49,35 +51,31 @@ function fruitPriceUpdates() {
 		if(randomNumber(1, 2) == 1 && (fruitPrices[i][1] <= 900)){
 			fruitPrices[i][1] += 50;
 			fruitPrices[i][2] = "increase";
-			// console.log("increase");
 		} else if (fruitPrices[i][1] > 50) {
 			fruitPrices[i][1] -= 50;
 			fruitPrices[i][2] = "decrease";
-			// console.log("decrease");
 		};
 	}
-	$('.apple > .price').text("$" + fruitPrices[0][1]);
-	$('.orange > .price').text("$" + fruitPrices[1][1]);
-	$('.banana > .price').text("$" + fruitPrices[2][1]);
-	$('.pear > .price').text("$" + fruitPrices[3][1]);
-// console.log("pears price: " + fruitPrices[1][1] + "pears indicator: " + fruitPrices[1][2]);
+	$('.apple > .price').text("$" + formatNum(fruitPrices[0][1]));
+	$('.orange > .price').text("$" + formatNum(fruitPrices[1][1]));
+	$('.banana > .price').text("$" + formatNum(fruitPrices[2][1]));
+	$('.pear > .price').text("$" + formatNum(fruitPrices[3][1]));
 }
-fruitPriceUpdates(fruitPrices);
 //this will call the fruitPriceUpdates function each 15 seconds
-setInterval('fruitPriceUpdates(fruitPrices);', 1000);
+setInterval('fruitPriceUpdates(fruitPrices);', 15000);
 
 $(document).ready(function(){
-
-	$('.cash').text("$" + User.cash);
+	fruitPriceUpdates(fruitPrices);
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	$(".buy-apple").on('click', function(){
 		User.numCurrentApples++;
 		User.totalNumApples++;
 		User.spentApples += fruitPrices[0][1];
 		User.cash -= fruitPrices[0][1];
 	var avgApple = User.spentApples / User.totalNumApples;
-	$('.userapple > .inventory').text(User.numCurrentApples);
-	$('.userapple > .avgprice').text(avgApple);
-	$('.cash').text("$" + User.cash);
+	$('.userapple:nth-child(2)').text(User.numCurrentApples);
+	$('.userapple:nth-child(3)').text(formatNum(avgApple));
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".buy-orange").on('click', function(){
@@ -85,10 +83,10 @@ $(document).ready(function(){
 		User.totalNumOranges++;
 		User.spentOranges += fruitPrices[1][1];
 		User.cash -= fruitPrices[1][1];
-	var avgOrange = User.spentOrange / User.totalNumOranges;
-	$('.userorange > .inventory').text(User.numCurrentOranges);
-	$('.userorange > .avgprice').text(avgOrange);
-	$('.cash').text("$" + User.cash);
+	var avgOrange = User.spentOranges / User.totalNumOranges;
+	$('.userorange:nth-child(2)').text(User.numCurrentOranges);
+	$('.userorange:nth-child(3)').text(formatNum(avgOrange));
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".buy-banana").on('click', function(){
@@ -97,9 +95,9 @@ $(document).ready(function(){
 		User.spentBananas += fruitPrices[2][1];
 		User.cash -= fruitPrices[2][1];
 	var avgBanana = User.spentBananas / User.totalNumBananas;
-	$('.userbanana > .inventory').text(User.numCurrentBananas);
-	$('.userbanana > .avgprice').text(avgBanana);
-	$('.cash').text("$" + User.cash);
+	$('.userbanana:nth-child(2)').text(User.numCurrentBananas);
+	$('.userbanana:nth-child(3)').text(formatNum(avgBanana));
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".buy-pear").on('click', function(){
@@ -108,37 +106,37 @@ $(document).ready(function(){
 		User.spentPears += fruitPrices[3][1];
 		User.cash -= fruitPrices[3][1];
 	var avgPear = User.spentPears / User.totalNumPears;
-	$('.userpear > .inventory').text(User.numCurrentPears);
-	$('.userpear > .avgprice').text(avgPear);
-	$('.cash').text("$" + User.cash);
+	$('.userpear:nth-child(2)').text(User.numCurrentPears);
+	$('.userpear:nth-child(3)').text(formatNum(avgPear));
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".sell-apple").on('click', function(){
 		User.numCurrentApples--;
 		User.cash += fruitPrices[0][1];
-	$('.userapple > .inventory').text(User.numCurrentApples);
-	$('.cash').text("$" + User.cash);
+	$('.userapple:nth-child(2)').text(User.numCurrentApples);
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".sell-orange").on('click', function(){
 		User.numCurrentOranges--;
 		User.cash += fruitPrices[1][1];
-	$('.userorange > .inventory').text(User.numCurrentOranges);
-	$('.cash').text("$" + User.cash);
+	$('.userorange:nth-child(2)').text(User.numCurrentOranges);
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".sell-banana").on('click', function(){
 		User.numCurrentBananas--;
 		User.cash += fruitPrices[2][1];
-	$('.userbanana > .inventory').text(User.numCurrentBananas);
-	$('.cash').text("$" + User.cash);
+	$('.userbanana:nth-child(2)').text(User.numCurrentBananas);
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 	
 	$(".sell-pear").on('click', function(){
 		User.numCurrentPears--;
 		User.cash += fruitPrices[3][1];
-	$('.userpear > .inventory').text(User.numCurrentPears);
-	$('.cash').text("$" + User.cash);
+	$('.userpear:nth-child(2)').text(User.numCurrentPears);
+	$('.cash').text("You have $" + formatNum(User.cash) + " in your bank.");
 	});
 
 }); 
