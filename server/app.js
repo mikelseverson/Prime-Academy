50696946ae6fa68132ed257ef909236e8fdc93cc
Mikel Severson <mikelseverson@Mikels-MacBook-Air.local>
2015-07-29 11:36:58 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/views/index.html"))
+});
